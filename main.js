8bb7c7c22456e1ef062812c7d7e7bb8eff4a72b2
MarcioRMed <contato.microinfo@gmail.com>
2022-06-07 16:58:15 -0300

    Mission 4 - Ok

--- main.js
+++ main.js
@@ -19,24 +19,23 @@ for(const link of links){
   })
 }
 
-// fim Menu ============================
 
 // modifica o header da página quando der scroll
+function changeHeaderWhenScroll(){
+  
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function(){
   if(window.scrollY >= navHeight){
     // scroll é maior que a altura do header
     header.classList.add('scroll')
-
+  
   }else{
     // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
 
+}
 
 // testimonial slider - swiper - lib carrossel 
 
@@ -64,5 +63,27 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, {interval: 100})
+
+
+  // Botão voltar para o topo
+
+  function backToTop(){
+    const backToTopButton = document.querySelector('.back-to-top')
+    
+    if(window.scrollY >= 560){
+      backToTopButton.classList.add('show')
+  
+    } else{
+      backToTopButton.classList.remove('show')
+    }
+  }
+ 
+
+  // When Scroll
+window.addEventListener('scroll', function(){
+  changeHeaderWhenScroll()
+  backToTop()
+})
